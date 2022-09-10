f60d2377cd9cdeb05d82dd0409430387a777f349
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2022-09-10 20:43:01 +0200

    docs: add tsdoc examples (#35)
    Closes #34

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -2,11 +2,30 @@ export type LikeLanguageCode = string | number | symbol;
 
 /**
  * Used to wrap each definition of translation options.
+ *
+ * @example
+ * ```
+ * type tOptions = TranslationOptions<{
+ *   name: string;
+ * }>;
+ * ```
  */
 export type TranslationOptions<T> = Readonly<T>;
 
 /**
  * Used to implement the `TranslationOptions`.
+ *
+ * @example
+ * ```
+ * const tMap: TranslationMap<"de" | "en", tOptions> = {
+ *   de: {
+ *     name: (options) => options.name,
+ *   },
+ *   en: {
+ *     name: (options) => options.name,
+ *   },
+ * };
+ * ```
  */
 export type TranslationMap<
   L extends LikeLanguageCode,
