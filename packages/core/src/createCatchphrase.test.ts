40716c2a88e59bcbd059c668cfdd1b7f60dbe138
Bikossor <andre.lichtenthaeler@gmail.com>
2022-12-26 13:24:25 +0100

    test: add tests for `getTranslations` function
    #89

--- packages/core/src/createCatchphrase.test.ts
+++ packages/core/src/createCatchphrase.test.ts
@@ -3,11 +3,34 @@ import { createCatchphrase } from "./createCatchphrase";
 describe("createCatchphrase", () => {
   type LanguageCode = "de" | "en";
 
+  type TranslationType = {
+    Greeting: (name: string) => string;
+  };
+
   it("should set the default language", () => {
     const { getDefaultLanguage } = createCatchphrase<LanguageCode>({
       defaultLanguage: "de",
     });
 
     expect(getDefaultLanguage()).toStrictEqual<LanguageCode>("de");
+  });
+
+  it("should translate a string using the defaultLanguage", () => {
+    const { createTranslation } = createCatchphrase<LanguageCode>({
+      defaultLanguage: "de",
+    });
+
+    const { getTranslations } = createTranslation<TranslationType>(() => ({
+      de: {
+        Greeting: (name) => `Hallo ${name}`,
+      },
+      en: {
+        Greeting: (name) => `Hello ${name}`,
+      },
+    }));
+
+    const { Greeting } = getTranslations();
+
+    expect(Greeting("André")).toStrictEqual("Hallo André");
   });
 });
