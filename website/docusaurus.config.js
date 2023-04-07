5076e475d4da7a6ad039c6728491b0c1cb8c42a2
Bikossor <andre.lichtenthaeler@gmail.com>
2023-04-07 13:31:27 +0200

    feat: change code theme to vscode

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,8 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const theme = require("prism-react-renderer/themes/vsDark");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -111,8 +110,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} André Lichtenthäler. Built with Docusaurus.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme,
+        darkTheme: theme,
       },
     }),
 };
